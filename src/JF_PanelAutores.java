458324972b55e2028eb483614be053ee4608dba6
DaCaHo-tupadre <dacaho2004@alumnes.ilernalleida.com>
2023-04-18 17:42:21 +0200

    JF_Menu
    Hacer menussss

--- src/JF_PanelAutores.java
+++ src/JF_PanelAutores.java
@@ -1,6 +1,10 @@
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.RenderingHints;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
@@ -8,6 +12,7 @@ import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
+import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -17,32 +22,49 @@ import javax.swing.SwingUtilities;
 
 public class JF_PanelAutores extends JPanel implements ActionListener {
 	JButton Atras;
-	JLabel jLabel2;
-	
+	Dimension screenSize;
 	JF_PanelAutores()  {
 	
-		setLayout(new BorderLayout());
+		setLayout(null);
+
+		this.screenSize=Toolkit.getDefaultToolkit().getScreenSize();
 		
 		this.Atras = new JButton("Atras");
 		
 		BufferedImage imgDavid;
 		try {
 			imgDavid = ImageIO.read(new File("imagenes/fotoDavid.png"));
-			Image scaledInstance = imgDavid.getScaledInstance(200, 100, Image.SCALE_DEFAULT);
-			jLabel2.setIcon(new ImageIcon(scaledInstance));
-			//JLabel imagenDavid = new JLabel(new ImageIcon(imgDavid));
-			//imagenDavid.resize(200,200);
-			//SetImageSize(12);
-			add(jLabel2);
-			
+			//Image scaledInstance = imgDavid.getScaledInstance(2, 1, Image.SCALE_SMOOTH);
+			//jLabel2.setIcon(new ImageIcon(scaledInstance));
+			JLabel imagenDavid = new JLabel(tamaño(new ImageIcon(imgDavid),300,200));
+			//imagenDavid
+			Dimension sizeImgDavid = imagenDavid.getPreferredSize();
+			imagenDavid.setBounds( 200,200, sizeImgDavid.width,sizeImgDavid.height);
+			add(imagenDavid);
+			//imagenDavid.setLocation(50,500);
 		} catch (IOException e) {
 			System.out.print("ds");
 		}
 		
-		
-		add(this.Atras, BorderLayout.SOUTH);
+	
+		int x = screenSize.height-300;
+		Atras.setBounds(0,screenSize.height-60,screenSize.width,20);
+		add(this.Atras);
 		this.Atras.addActionListener(this);
 	}
+	
+	
+	
+	
+	public static ImageIcon tamaño(ImageIcon im, int w, int h) {
+		BufferedImage bi = new BufferedImage(w,h, BufferedImage.TRANSLUCENT);
+		Graphics2D gd=(Graphics2D)bi.createGraphics();
+		gd.addRenderingHints(new RenderingHints(RenderingHints.KEY_RENDERING,RenderingHints.VALUE_RENDER_QUALITY));
+		gd.drawImage(im.getImage(),0,0,w,h,null);
+		gd.dispose();
+		return new ImageIcon(bi);
+	}
+	
 
 
 	@Override
