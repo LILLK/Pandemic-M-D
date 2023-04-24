974c98982d1ed23b3b4905c0cf42179a1951cc9d
DaCaHo-tupadre <dacaho2004@alumnes.ilernalleida.com>
2023-04-24 21:13:36 +0200

    Revert "ADA"
    This reverts commit 27eb8f811b4d2ab4f6e08e2b260eaf5146c2b657.

--- src/Pandemic/JF_PanelVersion.java
+++ src/Pandemic/JF_PanelVersion.java
@@ -2,25 +2,18 @@ package Pandemic;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
-import java.awt.Graphics;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
 
-import javax.imageio.ImageIO;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
-import Botones.BotonAtras;
-
-public class JF_PanelVersion extends JPanel {
-	BotonAtras Atras;
+public class JF_PanelVersion extends JPanel implements ActionListener {
+	JButton Atras;
 
 	Dimension screenSize;
 	
@@ -29,34 +22,25 @@ public class JF_PanelVersion extends JPanel {
 		setLayout(null);
 		this.screenSize=Toolkit.getDefaultToolkit().getScreenSize();
 		
-		this.Atras = new BotonAtras(this);
+		this.Atras = new JButton("Atras");
 		JLabel Version = new JLabel("Version 1.0");
 		
-		Atras.setBounds(0,(this.screenSize.height-((screenSize.width/20)+4)),this.screenSize.width,(screenSize.width/25));
+		this.Atras.setBounds(0,this.screenSize.height-60,this.screenSize.width,20);
 		add(this.Atras);
 		add(Version);
+		this.Atras.addActionListener(this);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
 		
-	}
-	public void irMenu() {
-		JFrame marcoP = (JFrame)SwingUtilities.getWindowAncestor(this);	
-		marcoP.remove(this);
-		marcoP.add(new JF_PanelMenu());
-		marcoP.setVisible(true);
-	}
-	public void paint(Graphics g) {
-		BufferedImage imgFondo;
-		try {
-			Dimension size =getSize();
-			imgFondo = ImageIO.read(new File("imagenes/imagenPandemic.png"));
-			g.drawImage(imgFondo, 0, 0, size.width, size.height,null);
-			setOpaque(false);
-			super.paint(g);
-		} catch (IOException e) {
-			System.out.print("Error IMGfondo JF_PanelMenuMenu");
+		if(e.getSource() == this.Atras) {
+			
+			JFrame marcoP = (JFrame)SwingUtilities.getWindowAncestor(this);	
+			marcoP.remove(this);
+			marcoP.add(new JF_PanelMenu());
+			marcoP.setVisible(true);
 		}
 		
-		
-	} 
-
-	
 	}
+}
