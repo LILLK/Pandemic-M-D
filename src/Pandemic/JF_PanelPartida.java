fcc14a9d3eafac8bb7c1a2ef021564c84b2e5b31
LILLK <tahrat.mehdi.co@gmail.com>
2023-05-02 02:58:10 +0200

    a
    a

--- src/Pandemic/JF_PanelPartida.java
+++ src/Pandemic/JF_PanelPartida.java
@@ -19,20 +19,45 @@ public class JF_PanelPartida extends JPanel {
 		screenSize = new Dimension(Toolkit.getDefaultToolkit().getScreenSize());
 
 		cambiarImg img = new cambiarImg();
+	
+		
 		JLabel mapa = new JLabel();
 		
 		mapa.setIcon(img.tamaño(new ImageIcon("imagenes/mapa_mundo.png"), 1550, 850));
-		mapa.setBounds(this.screenSize.width/2-(1550/2), this.screenSize.height/2-(850/2), 1550, 850);
+		mapa.setBounds(this.screenSize.width/2-(1550/2),
+				this.screenSize.height/2-(850/2), 
+				1550, 
+				850);
 		mapa.setVisible(true);
 		mapa.setOpaque(true);
 		
-		JPanel arriba = new JPanel();
-		arriba.setBackground(Color.black);
-		arriba.setBounds(1550,1550,1550,1550);
+		JLabel izquierda = new JLabel();
+
+		izquierda.setIcon(img.tamaño(new ImageIcon("imagenes/empty.png"), this.screenSize.width/2-850/2, this.screenSize.height));
+		izquierda.setBackground(Color.DARK_GRAY);
+		izquierda.setBounds(10,10,10,10);
+		izquierda.setOpaque(true);
+		izquierda.setVisible(true);
 		
-		//add(mapa);
-		add(arriba);
+		add(izquierda);
 	}
+	
+
+	public void paint(Graphics g) {
+		BufferedImage imgFondo;
+		try {
+			Dimension size = getSize();
+			imgFondo = ImageIO.read(new File("imagenes/white.png"));
+			//g.drawImage(imgFondo, 0, 0, size.width, size.height, null);
+			g.fillRect(0, 0, size.width, size.height);
+			setOpaque(false);
+			super.paint(g);
+		} catch (IOException e) {
+			System.out.print("Error IMGfondo JF_PanelInicio");
+		}
+
+	}
+
 	
 }
 
