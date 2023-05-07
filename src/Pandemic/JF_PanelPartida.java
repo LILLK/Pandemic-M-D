9dd046ac6ff073e65e552c3425d402134524f3fa
LILLK <tahrat.mehdi.co@gmail.com>
2023-05-08 01:07:03 +0200

    a
    a

--- src/Pandemic/JF_PanelPartida.java
+++ src/Pandemic/JF_PanelPartida.java
@@ -19,7 +19,6 @@ import Logico.Partida;
 
 public class JF_PanelPartida extends JPanel {
 
-	
 	BotonSalir Salir;
 	BotonCiudad BotonCiudad;
 	JFrame marcoP;
@@ -28,59 +27,38 @@ public class JF_PanelPartida extends JPanel {
 	JTextField JTFContraseña;
 	JF_PanelPartidaVacunas JF_PanelPartidaVacunas;
 
-	
-
 	public JF_PanelPartida() {
-		
-		
+
 		cambiarImg img = new cambiarImg();
 
-		Color color =new Color(71, 161, 197);
-		Border borde = BorderFactory.createLineBorder(Color.BLACK,3);
+		Color color = new Color(71, 161, 197);
+		Border borde = BorderFactory.createLineBorder(Color.BLACK, 3);
 		screenSize = new Dimension(Toolkit.getDefaultToolkit().getScreenSize());
 		setLayout(null);
 
-		
-		//labels//
-		
+		// labels//
+		JF_PanelPartidaVacunas = new JF_PanelPartidaVacunas();
 		JLabel mapa = new JLabel();
-		mapa.setIcon(img.tamaño(new ImageIcon("imagenes/mapa_2.png"), 1550, 850));
-		mapa.setBounds(1,
-				1, 
-				1550, 
-				850);
-		mapa.setVisible(true);
-		mapa.setOpaque(true);
-		
-		
 
-		JLabel derechaAriba = new  JLabel();
-		derechaAriba.setBounds(1551,
-				1, 
-				(this.screenSize.width-1550), 
-				this.screenSize.height/2);
+		mapa.setIcon(img.tamaño(new ImageIcon("imagenes/mapa_2.png"), 1550, 850));
+		mapa.setBounds(1, 1, 1550, 850);
+
+		JLabel derechaAriba = new JLabel();
+		derechaAriba.setBounds(1551, 1, (this.screenSize.width - 1550), this.screenSize.height / 2);
 		derechaAriba.setBackground(color);
 		derechaAriba.setBorder(borde);
 		derechaAriba.setOpaque(true);
+		JLabel puntuacion = new JLabel();
+
 		
 		
-		
-		
-		
-		JLabel derechaAbajo = new  JLabel();
-		derechaAbajo.setBounds(1551,
-				this.screenSize.height/2+1, 
-				(this.screenSize.width-1550), 
-				(this.screenSize.height/2)-23);
+		JLabel derechaAbajo = new JLabel();
+		derechaAbajo.setBounds(1551, this.screenSize.height / 2 + 1, (this.screenSize.width - 1550),
+				(this.screenSize.height / 2) - 23);
 		derechaAbajo.setBackground(color);
 		derechaAbajo.setBorder(borde);
 		derechaAbajo.setOpaque(true);
-		
-		JF_PanelPartidaVacunas = new JF_PanelPartidaVacunas();
-		
-		
-		
-		
+
 		
 		
 		establecerBotonesCiudades();
@@ -89,26 +67,24 @@ public class JF_PanelPartida extends JPanel {
 		}
 		for (Ciudades ciudad : Partida.ciudades) {
 			add(ciudad.boton);
-		}	
-		
-		
- 		add(derechaAriba);
+		}
+
+		add(derechaAriba);
 		add(derechaAbajo);
 		add(JF_PanelPartidaVacunas);
 		add(mapa);
-		
-		
-		
-	;
-		
+
+		;
+
 	}
+
 	public String getJTFUsuario() {
 		return JTFUsuario.getText();
 	}
+
 	public String getJTFContraseña() {
 		return JTFContraseña.getText();
 	}
-
 
 	public void paint(Graphics g) {
 		BufferedImage imgFondo;
@@ -130,28 +106,28 @@ public class JF_PanelPartida extends JPanel {
 		marcoP.add(new JF_PanelRegistrarse());
 		marcoP.setVisible(true);
 	}
+
 	public void irMenu() {
-		JFrame marcoP = (JFrame)SwingUtilities.getWindowAncestor(this);	
+		JFrame marcoP = (JFrame) SwingUtilities.getWindowAncestor(this);
 		marcoP.remove(this);
 		marcoP.add(new JF_PanelMenu());
 		marcoP.setVisible(true);
 	}
+
 	/////////////////////////////////////////////////////////////////////////
-	///-establecerBotonesCiudades
-	///Coloca las ciudades en su lugar
+	/// -establecerBotonesCiudades
+	/// Coloca las ciudades en su lugar
 	////////////////////////////////////////////////////////////////////////
 	public static void establecerBotonesCiudades() {
 		int tamañoX = 30;
 		int tamañoY = 20;
 		for (Ciudades ciudad : Partida.ciudades) {
-			ciudad.boton.setBounds(ciudad.posicionX-(tamañoX/2), ciudad.posicionY-(tamañoY/2),tamañoX,tamañoY);
+			ciudad.boton.setBounds(ciudad.posicionX - (tamañoX / 2), ciudad.posicionY - (tamañoY / 2), tamañoX,
+					tamañoY);
 			ciudad.boton.setVisible(true);
 			ciudad.boton.setOpaque(false);
-			ciudad.boton.JLNombreCiudad.setBounds(ciudad.posicionX-(50), ciudad.posicionY+(20), 100, 20);
+			ciudad.boton.JLNombreCiudad.setBounds(ciudad.posicionX - (50), ciudad.posicionY + (20), 100, 20);
 		}
 	}
-	
-
-	
 
 }
