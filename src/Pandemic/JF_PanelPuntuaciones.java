0335cc8c6e56899e5d71318ebcddfce3ef795d2b
DaCaHo-tupadre <dacaho2004@alumnes.ilernalleida.com>
2023-05-08 03:09:23 +0200

    Dying Light
    Dying Light

--- src/Pandemic/JF_PanelPuntuaciones.java
+++ src/Pandemic/JF_PanelPuntuaciones.java
@@ -43,7 +43,7 @@ public class JF_PanelPuntuaciones extends JPanel {
 	private static final String USER = "DAW_PNDC22_23_DAME";
 	private static final String PWD = "DM123";
 	// Si estáis desde casa, la url será oracle.ilerna.com y no 192.168.3.26
-	private static final String URL = "jdbc:oracle:thin:@192.168.3.26:1521:xe";
+	private static final String URL = "jdbc:oracle:thin:@oracle.ilerna.com:1521:xe";
 	public JF_PanelPuntuaciones() {
 		
 		JF_PanelPuntuaciones.rankings = new ArrayList<Rankings>();
